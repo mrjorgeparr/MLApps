0c156fad6962f33a97f8209978afa820de6f899e
sbkit <sven.bulach96gmail.com>
2023-05-06 19:28:43 +0200

    Increased search space

--- src/machine_learning/logistic_regression_classifier.py
+++ src/machine_learning/logistic_regression_classifier.py
@@ -3,11 +3,12 @@ from sklearn.linear_model import LogisticRegression
 import numpy as np
 
 class LogisticRegressionClassifier(BaseClassifier):
-    def __init__(self, max_iter=5000, random_state=42, class_weight=None):
+    def __init__(self, max_iter=5000, random_state=42, class_weight=None, n_iter=50):
         classifier = LogisticRegression(max_iter=max_iter, multi_class='auto', class_weight=class_weight)
         param_grid = {
             'C': np.logspace(-3, 3, 7),
-            'penalty': ['l2'],
-            'solver': ['liblinear', 'saga']
+            'penalty': ['l1', 'l2', 'elasticnet'],
+            'solver': ['liblinear', 'saga'],
+            'l1_ratio': np.linspace(0, 1, 6)  # only used when penalty='elasticnet'
         }
-        super().__init__(classifier, param_grid, random_state)
+        super().__init__(classifier, param_grid, random_state, n_iter=n_iter)
